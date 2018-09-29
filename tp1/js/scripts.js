52eec28c6604f1810bcc7143aefd3a0bd3d780c2
Pyglidden <pierreyves.glidden@hotmail.com>
2018-09-29 10:26:56 -0400

    finalisation pour TP

--- tp1/js/scripts.js
+++ tp1/js/scripts.js
@@ -1,4 +1,6 @@
 $(function() {
+    $("#header").load("header.html"); 
+    $("#footer").load("footer.html"); 
     $(window).scroll(function() {
     var max_width = 990;   
     if ($(window).width() < max_width) {
